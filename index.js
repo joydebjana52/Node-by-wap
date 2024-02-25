456f023fef6b2a0ed372b37ce61da49d35f008a3
joydebjana52 <joydebjana52@gmail.com>
2024-02-25 09:03:07 +0530

    *

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const server = http.createServer(function (req, res) {
     const requestMethod = req.method;
     console.log(requestMethod);
 
+    if (requestMethod === "GET") {
+        res.statusCode = 200;
+        res.end('GET request - User wants to read some data');
+    } else if (requestMethod === "POST") {
+        res.statusCode = 201;
+        res.end('POST request - User wants to add store some data');
+    } else if (requestMethod === "PUT") {
+        res.statusCode = 201;
+        res.end("PUT request - User wants to update data");
+    } else if (requestMethod === "DELETE") {
+        res.statusCode = 207;
+        res.end('DELETE request - User want to delete data');
+    } else {
+        res.statusCode = 405;
+        res.end('Unknown request - User wants to hack the server');
+    }
 
-
-    res.statusCode = 200;
-    res.end("Success");   //? This helps to follow the "stateless protocol".
+    // res.statusCode = 200;
+    // res.end("Success");   //? This helps to follow the "stateless protocol".
 });
 
 server.listen(8000);
